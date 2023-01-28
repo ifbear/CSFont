ae8192885e7c020752dd7fc23c46b8193e6d82f1
dexiong <dexiong@staff.sina.com.cn>
2023-01-28 16:03:32 +0800

    init

--- CSFont/ViewController.swift
+++ CSFont/ViewController.swift
@@ -14,6 +14,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        var cfBundle: CFBundle?
+        var resourceRequest: NSBundleResourceRequest?
+        let bundle = Bundle(for: type(of: self))
+        resourceRequest = NSBundleResourceRequest(tags: Set(arrayLiteral: "fonts"), bundle: bundle)
+        cfBundle = CFBundleCreate(kCFAllocatorDefault, bundle.bundleURL as CFURL)
+        resourceRequest?.beginAccessingResources(completionHandler: { error in
+            if let error = error {
+                print(error)
+            } else {
+                var fonts: [String] = []
+                let weights: [String] = ["_Black", "_Bold", "_Light", "_Medium", "_Regular", "_Thin"]
+                let types: [String] = ["", "_SC", "_TC", "_Naskh_Arabic_UI", "_Naskh_Arabic", "_Italic", "_Condensed_Italic", "_Condensed"]
+                for type in types {
+                    for weight in weights {
+                        fonts.append("HarmonyOS_Sans\(type)\(weight)")
+                    }
+                }
+                CTFontManagerRegisterFontsWithAssetNames(fonts as CFArray, cfBundle, .persistent, true) { errors, finish in
+                    print(errors, finish)
+                    return finish
+                }
+            }
+        })
+    }
 
 }
 
